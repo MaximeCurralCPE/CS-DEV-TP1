8c9c13b5eb98d8e4b709aadb54e84bfff290435a
Maxime CURRAL <maxime.curral@cpe.fr>
2020-11-24 08:47:58 +0100

    démarrage TP1

---
+++ calendrier.py
@@ -0,0 +1,11 @@
+#TP1 CS-DEV
+#24/11/20
+#Maxime Curral
+
+import calendrier_bibliothèque as lib
+
+#question 1
+print("question 1 :",lib.test_bissextile(2020))
+
+#question 2
+print("question 2 :",lib.nombre_jours(2,2021))
---
+++ calendrier_bibliothèque.py
@@ -0,0 +1,26 @@
+#TP1 CS-DEV
+#24/11/20
+#Maxime Curral
+
+def test_bissextile(annee):
+    #une année est bissextile si elle est divisible par 4 et ne termine pas par 00
+    #cette fonction teste ces deux conditions et renvoie True si l'année est bissextile.
+    if str(annee)[-2:] != 0 and annee % 4 == 0:
+        return True
+    else:
+        return False
+
+def nombre_jours(mois,annee):
+    #vérifie si le mois donné en entrée existe
+    #s'il existe, retourne le nombre de jours de ce mois
+    if mois >= 1 and mois <= 12 and annee >= 0:
+        if mois in [1,3,5,7,8,10,12]:
+            return 31
+        elif mois in [4,6,9,11]:
+            return 30
+        elif test_bissextile(annee) == True:
+            return 29
+        else:
+            return 28
+    else:
+        return False
