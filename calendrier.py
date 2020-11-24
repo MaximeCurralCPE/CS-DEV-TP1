0f65a3722807ddd4eeba0aca1df02daa8421f553
Maxime CURRAL <maxime.curral@cpe.fr>
2020-11-24 09:28:47 +0100

    ajout des dernières réponses

--- calendrier.py
+++ calendrier.py
@@ -8,4 +8,18 @@ import calendrier_bibliothèque as lib
 print("question 1 :",lib.test_bissextile(2020))
 
 #question 2
-print("question 2 :",lib.nombre_jours(2,2021))
+print("question 2 :",lib.nombre_jours(11,2020))
+
+#question 3
+print("question 3 :",lib.check_date('21/12/2012'))
+
+#question 4
+print("question 4 :")
+print("Bonjour, veuillez suivre les instructions à l'écran pour vérifier si votre date existe :")
+jour = input("  entrez le jour sous forme de nombre entier :")
+mois = input("  entrez le mois sous forme de nombre entier :")
+annee = input("  entrez l'année sous forme de nombre entier :")
+if lib.check_date(jour + '/' + mois + '/' + annee) == True:
+    print("date valide")
+else :
+    print("date non valide")
--- calendrier_bibliothèque.py
+++ calendrier_bibliothèque.py
@@ -5,20 +5,35 @@
 def test_bissextile(annee):
     #une année est bissextile si elle est divisible par 4 et ne termine pas par 00
     #cette fonction teste ces deux conditions et renvoie True si l'année est bissextile.
-    if str(annee)[-2:] != 0 and annee % 4 == 0:
+    if str(annee)[-2:] != 0 and int(annee) % 4 == 0:
         return True
+    else:
+        return False
+def check_date(date):
+    jour,mois,annee = map(int, date.split('/'))
+    if jour > 0:
+        if annee >= 0:
+            if mois >= 1 and mois <= 12:
+                if mois == 2 and test_bissextile(annee) == True and jour <= 29:
+                    return True
+                elif mois == 2 and test_bissextile(annee) == False and jour <= 28:
+                    return True
+                elif mois in [1,3,5,7,8,10,12] and jour <= 31:
+                    return True
+                elif mois in [4,6,9,11] and jour <= 30:
+                    return True
     else:
         return False
 
 def nombre_jours(mois,annee):
     #vérifie si le mois donné en entrée existe
     #s'il existe, retourne le nombre de jours de ce mois
-    if mois >= 1 and mois <= 12 and annee >= 0:
-        if mois in [1,3,5,7,8,10,12]:
-            return 31
-        elif mois in [4,6,9,11]:
-            return 30
-        elif test_bissextile(annee) == True:
+    if mois in [1,3,5,7,8,10,12]:
+        return 31
+    elif mois in [4,6,9,11]:
+        return 30
+    elif mois == 2:
+        if test_bissextile(annee) == True:
             return 29
         else:
             return 28
